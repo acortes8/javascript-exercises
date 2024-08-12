64f5a01a821683edb2ae32e283fb0d47936e0d37
acortes8 <46253800+acortes8@users.noreply.github.com>
2024-08-12 18:58:01 -0500

    Complete JS exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * (5/9); //formula for F to C
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = (celsius * (9/5) + 32); //formula for C to F
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
